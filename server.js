d2c3d2c30a6057a46640d88381a6d7fb09bdad42
angelr1076 <angelr1076@gmail.com>
2024-11-13 07:04:41 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // set port
@@ -7,11 +7,11 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 // routes
-app.get("/", function(req, res) {
-  res.render("index");
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
 // Run with "node server.js" command to run locally
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`app running on port ${port}`);
 });
